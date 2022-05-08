13477f5eb5531780ecaeab9a628d60648ea1260b
Kgethi <kgethi47@gmail.com>
2022-05-08 13:47:29 +0200

    Update head.php
    deleted the lottie file script

--- includes/head.php
+++ includes/head.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageName ?></title>
 
-    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+  
     <link rel="stylesheet" href="css/bootstrap.css">
     <script src="js/bootstrap.js">
     </script>
